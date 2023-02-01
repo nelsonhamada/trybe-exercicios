cce0e38b325eb926b02e4a20c12845d1d6c436ea
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-02-01 18:03:44 -0300

    Tentando modificar e adicionar pelo .document

---
+++ fundamentos/secao-04-dom-e-seletores/dia-02/script.js
@@ -0,0 +1,7 @@
+const inicio = document.getElementById('elementoOndeVoceEsta');
+const pai = inicio.parentElement;
+pai.style.color = 'blue';
+const primeiroFilho = inicio.firstElementChild;
+primeiroFilho.innerHTML = 'text';
+
+
