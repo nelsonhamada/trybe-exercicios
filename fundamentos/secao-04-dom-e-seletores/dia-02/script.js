fc948d8271f60b658323924e40e84607969c6b3b
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-02-03 10:46:05 -0300

    revisando conteúdo de DOM

--- fundamentos/secao-04-dom-e-seletores/dia-02/script.js
+++ fundamentos/secao-04-dom-e-seletores/dia-02/script.js
@@ -1,7 +1,32 @@
-const inicio = document.getElementById('elementoOndeVoceEsta');
-const pai = inicio.parentElement;
-pai.style.color = 'blue';
-const primeiroFilho = inicio.firstElementChild;
-primeiroFilho.innerHTML = 'text';
+// // Crie um irmão para elementoOndeVoceEsta.
+
+// const pai = document.getElementById('pai');
+// const irmaoElementoOndeVoceEsta = document.createElement('section');
+// irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+// pai.appendChild(irmaoElementoOndeVoceEsta);
+
+// // Crie um filho para elementoOndeVoceEsta.
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// const filhoElementoOndeVoceEsta = document.createElement('section');
+// filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+// elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+// // Crie um filho para primeiroFilhoDoFilho.
+// const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+// const filhoPrimeiroFilhoDoFilho = document.createElement('section');
+// filhoPrimeiroFilhoDoFilho.id ='filhoPrimeiroFilhoDoFilho';
+// primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+
+// // A partir desse filho criado, acesse terceiroFilho.
+// filhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling
+
+
+//  2  -
+
+const elementoASerRemovido = document.getElementById('elementoOndeVoceEsta');
+// elementoASerRemovido.removeChild(elementoASerRemovido.firstElementChild);
+// outra alternativa
+elementoASerRemovido.firstElementChild.remove();
+
 
 
