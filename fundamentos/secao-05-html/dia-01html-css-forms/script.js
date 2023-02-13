e521afebfe2f29a15ebf8965a4703e90bcc51613
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-02-13 09:47:41 -0300

    Criando diretório para secao 05 dia 04.

---
+++ fundamentos/secao-05-html/dia-01html-css-forms/script.js
@@ -0,0 +1,12 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+
+
+INPUT_TEXT.addEventListener('keypress',(event) => {
+const character = event.key;
+if (character !== 'a') {
+    event.preventDefault();
+}
+}) 
